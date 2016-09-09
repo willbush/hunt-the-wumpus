9b24cb2678df63469f83b424c501e5b4c33ddc69
William Bush <william.bush@utdallas.edu>
2016-09-08 21:58:07 -0500

    add basic console app

---
+++ hunt-the-wumpus/Program.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace hunt_the_wumpus
+{
+    class Program
+    {
+        private const string HUNT_ASCII = @"
+ ██░ ██  █    ██  ███▄    █ ▄▄▄█████▓                  
+▓██░ ██▒ ██  ▓██▒ ██ ▀█   █ ▓  ██▒ ▓▒                  
+▒██▀▀██░▓██  ▒██░▓██  ▀█ ██▒▒ ▓██░ ▒░                  
+░▓█ ░██ ▓▓█  ░██░▓██▒  ▐▌██▒░ ▓██▓ ░                   
+░▓█▒░██▓▒▒█████▓ ▒██░   ▓██░  ▒██▒ ░                   
+ ▒ ░░▒░▒░▒▓▒ ▒ ▒ ░ ▒░   ▒ ▒   ▒ ░░                     
+ ▒ ░▒░ ░░░▒░ ░ ░ ░ ░░   ░ ▒░    ░                      
+ ░  ░░ ░ ░░░ ░ ░    ░   ░ ░   ░                        
+ ░  ░  ░   ░              ░                            
+";
+        private const string THE_ASCII = @"
+▄▄▄█████▓ ██░ ██ ▓█████                                
+▓  ██▒ ▓▒▓██░ ██▒▓█   ▀                                
+▒ ▓██░ ▒░▒██▀▀██░▒███                                  
+░ ▓██▓ ░ ░▓█ ░██ ▒▓█  ▄                                
+  ▒██▒ ░ ░▓█▒░██▓░▒████▒                               
+  ▒ ░░    ▒ ░░▒░▒░░ ▒░ ░                               
+    ░     ▒ ░▒░ ░ ░ ░  ░                               
+  ░       ░  ░░ ░   ░                                  
+          ░  ░  ░   ░  ░                               
+";
+        private const string WUMPUS_ASCII = @"
+ █     █░█    ██  ███▄ ▄███▓ ██▓███   █    ██   ██████ 
+▓█░ █ ░█░██  ▓██▒▓██▒▀█▀ ██▒▓██░  ██▒ ██  ▓██▒▒██    ▒ 
+▒█░ █ ░█▓██  ▒██░▓██    ▓██░▓██░ ██▓▒▓██  ▒██░░ ▓██▄   
+░█░ █ ░█▓▓█  ░██░▒██    ▒██ ▒██▄█▓▒ ▒▓▓█  ░██░  ▒   ██▒
+░░██▒██▓▒▒█████▓ ▒██▒   ░██▒▒██▒ ░  ░▒▒█████▓ ▒██████▒▒
+░ ▓░▒ ▒ ░▒▓▒ ▒ ▒ ░ ▒░   ░  ░▒▓▒░ ░  ░░▒▓▒ ▒ ▒ ▒ ▒▓▒ ▒ ░
+  ▒ ░ ░ ░░▒░ ░ ░ ░  ░      ░░▒ ░     ░░▒░ ░ ░ ░ ░▒  ░ ░
+  ░   ░  ░░░ ░ ░ ░      ░   ░░        ░░░ ░ ░ ░  ░  ░  
+    ░      ░            ░               ░           ░  
+";
+
+        static void Main()
+        {
+            Thread.Sleep(500);
+            Console.WriteLine(HUNT_ASCII);
+            Thread.Sleep(500);
+            Console.WriteLine(THE_ASCII);
+            Thread.Sleep(500);
+            Console.WriteLine(WUMPUS_ASCII);
+            Console.ReadLine();
+        }
+    }
+}
